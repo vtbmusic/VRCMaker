907b2397a90e54d12b57dc01b917dbc92d0dacd2
Kurokitu <foxiti.a@gmail.com>
2020-06-11 15:01:14 +0800

    -fix- 修复脚本

--- build.cmd
+++ build.cmd
@@ -1,1 +1,3 @@
 pyinstaller --hidden-import=queue -w -i assets/icon/main.ico main.py
+
+xcopy assets dist\main\assets\ /e
--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #/bin/bash
 pyinstaller --hidden-import=queue -w -F -i assets/icon/main.ico main.py
+
+cp -r ./assets ./dist/
